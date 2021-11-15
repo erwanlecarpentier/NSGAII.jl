ef75404318de247a8c665d888645b8203aa89df9
erwanlecarpentier <erwanlecarpentier@mailbox.org>
2021-11-15 19:20:30 +0100

    CGP example with functions

--- examples/cgp_function_learning.jl
+++ examples/cgp_function_learning.jl
@@ -2,9 +2,13 @@ using NSGAII
 using CartesianGeneticProgramming
 using Cambrian
 using UnicodePlots
+using LinearAlgebra
+
+function out(plt)
+    println(IOContext(stdout, :color=>true), plt)
+end
 
 cfg = get_config("cfg/cgp.yaml")
-
 n_population = cfg.n_population
 n_gen = cfg.n_gen
 p_mut = 1.0 # Probability of mutation at each generation for each individual
@@ -12,12 +16,22 @@ plotevery = 100
 obj1, obj2 = Vector{Float64}(), Vector{Float64}()
 seeding_ind = Vector{Float64}() # Individuals for init
 
+x = [i for i in 0:0.1:1]
+f1(x) = 0.25 * (2.0 + cos(2.0*π*x))
+f2(x) = 0.25 * (2.0 + cos(2.5*π*x))
+y1 = f1.(x)
+y2 = f2.(x)
+out(lineplot([f1, f2], 0, 1, border=:dotted))
+
 # Fitness function (normalized)
 function my_fitness(chromosome::Vector{Float64})
-    o1 = 1.0 - sum(chromosome) / length(chromosome)
-    n = Int64(ceil(0.1*length(chromosome)))
-    o2 = sum(sort(chromosome)[1:n]) / n
-    #1.0 - o1, 1.0 - o2
+    ind = CGPInd(cfg, chromosome)
+    y_hat = zeros(length(y1))
+    @inbounds for i in eachindex(y1)
+        y_hat[i] = process(ind, [x[i]])[1]
+    end
+    o1 = norm(y1 - y_hat)
+    o2 = norm(y2 - y_hat)
     o1, o2
 end
 
@@ -56,7 +70,7 @@ function my_fplot(pop::AbstractArray)
 end
 
 # Run
-nsga(#_max(
+paretto = nsga(#_max(
     n_population, n_gen, my_fitness, my_init;
     pmut = p_mut,
     fmut = my_mutate!,#ind -> my_mutate!(ind.x),
@@ -70,3 +84,10 @@ nsga(#_max(
 # Plot
 println(IOContext(stdout, :color=>true), lineplot(obj1, title = "Objective 1", ylim=[0,1]))
 println(IOContext(stdout, :color=>true), lineplot(obj2, title = "Objective 2", ylim=[0,1]))
+
+# Plot all computed functions
+for i in eachindex(paretto)
+    ind = CGPInd(cfg, paretto[i].x)
+    f(x) = process(ind, [x])[1]
+    out(lineplot([f1, f2, f], 0, 1, border=:dotted))
+end
